1af804f31b41134ef7fa46dd157b5e72b7bd2997
Nishchay Maurya <nishchay.maurya@coindcx.com>
2022-08-21 15:45:30 +0530

    Minimal docs page created.

--- src/app/demo/better-bottomsheet-demo/better-bottomsheet-demo.module.ts
+++ src/app/demo/better-bottomsheet-demo/better-bottomsheet-demo.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule, Routes } from '@angular/router';
 import { BetterBottomsheetModule } from 'src/app/components/better-bottomsheet/better-bottomsheet.module';
 import { BetterBottomsheetPage } from './page/better-bottomsheet.page';
@@ -10,6 +11,13 @@ const routes: Routes = [{ path: '', component: BetterBottomsheetPage }];
 
 @NgModule({
   declarations: [BottomsheetContentComponent, BetterBottomsheetPage],
-  imports: [CommonModule, RouterModule.forChild(routes), BetterBottomsheetModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    BetterBottomsheetModule,
+    //Material Modules
+    MatButtonModule,
+    MatTabsModule,
+  ],
 })
 export class BetterBottomsheetDemoModule {}
